4284a0e0ed6e16a5fdeef21b92d2045453f57d4b
rayden <rayden@tut.by>
2026-02-01 03:59:21 +0300

    Show ERRORLEVEL when it is not equal to 0 after run cmd-script test.

--- cmd-tests/test-emptyJSON.cmd
+++ cmd-tests/test-emptyJSON.cmd
@@ -3,4 +3,8 @@ chcp 65001 > nul
 
 echo {} | java -XX:AOTCache=..\FFProbeChapters2Cue.aot -XX:+UseCompactObjectHeaders -jar ..\build\libs\FFProbeChapters2Cue.jar
 
+if %ERRORLEVEL% NEQ 0 (
+  echo ERRORLEVEL: %ERRORLEVEL%
+)
+
 pause
